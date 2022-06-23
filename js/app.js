70f245601f7e89685999bb4a2a39497a6bf0e68b
Yokke <mdembelepro@gmail.com>
2022-06-23 18:44:20 +0200

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,21 @@
+const idCitation = document.getElementById('id--citation')
+const citationQuote = document.getElementById('citation')
+const dice = document.querySelector('.dice')
+dice.addEventListener('click',() => {
+    const adviceSlipJsonAPI = fetch ('https://api.adviceslip.com/advice')
+
+    adviceSlipJsonAPI.then( response => {
+        if(response.ok)
+            return response.json()
+
+        else  
+            return 'Connexion failed'
+    })
+
+    .then( citation => {
+        
+        idCitation.textContent = citation['slip'].id
+        citationQuote.textContent = `" ${citation['slip'].advice} "`
+    })
+
+})
