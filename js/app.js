edfc2e4945b4c4d4bfe94afe53682642b4a387e4
Yokke <mdembelepro@gmail.com>
2022-06-25 18:22:20 +0200

    Now it's possible to send request more quickly

--- js/app.js
+++ js/app.js
@@ -1,8 +1,11 @@
 const idCitation = document.getElementById('id--citation')
 const citationQuote = document.getElementById('citation')
 const dice = document.querySelector('.dice')
+
+
+
 dice.addEventListener('click',() => {
-    const adviceSlipJsonAPI = fetch ('https://api.adviceslip.com/advice')
+    const adviceSlipJsonAPI = fetch ('https://api.adviceslip.com/advice',{cache:'no-cache'})
 
     adviceSlipJsonAPI.then( response => {
         if(response.ok)
